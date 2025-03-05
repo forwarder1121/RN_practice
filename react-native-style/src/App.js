2d789f558898f2e6e4099f73faaf706ba20cf455
김동환 <forwarder1121@naver.com>
2025-03-05 22:11:17 +0900

    Styled-component

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { viewStyles, textStyles } from "./styles";
 
 const App = () => {
     return (
-        <View>
-            <Text>React Native Style</Text>
+        <View style={styles.container}>
+            <Text style={[styles.text, { color: "green" }]}>
+                Inline Styleing -Text
+            </Text>
+            <Text style={[styles.text, styles.error]}>
+                Inline Styleing -Error
+            </Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        padding: 10,
+        fontSize: 26,
+        fontWeight: "600",
+        color: "black",
+    },
+    error: {
+        fontWeight: "400",
+        color: "red",
+    },
+});
 
 export default App;
