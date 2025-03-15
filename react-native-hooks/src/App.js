d4f6ab2d6e47b60a5c1ed9705528561601a0991e
김동환 <forwarder1121@naver.com>
2025-03-15 11:03:19 +0900

    hook - API

--- react-native-hooks/src/App.js
+++ react-native-hooks/src/App.js
@@ -5,6 +5,7 @@ import Counter from "./components/Counter";
 import Form from "./components/Form";
 import Button from "./components/Button";
 import Length from "./components/Length";
+import Dog from "./components/Dog";
 const Container = styled.View`
     flex: 1;
     background-color: #fff;
@@ -16,7 +17,7 @@ const App = () => {
     const [isVisible, SetIsVisible] = useState(true);
     return (
         <Container>
-            <Length />
+            <Dog />
         </Container>
     );
 };
