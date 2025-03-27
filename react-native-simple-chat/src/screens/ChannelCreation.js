74f88f16e63dfdfcd1d8f5237fe029f53d701c72
김동환 <forwarder1121@naver.com>
2025-03-27 11:51:35 +0900

    feat(channelcreation): channel creation logic implement

--- react-native-simple-chat/src/screens/ChannelCreation.js
+++ react-native-simple-chat/src/screens/ChannelCreation.js
@@ -1,8 +1,10 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import styled from "styled-components/native";
 import { Input, Button } from "../components";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-
+import { Alert } from "react-native";
+import { ProgressContext } from "../contexts";
+import { createChannel } from "../utils/firebase";
 const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.background};
@@ -20,7 +22,8 @@ const ErrorText = styled.Text`
     color: ${({ theme }) => theme.errorText};
 `;
 
-const ChannelCreation = () => {
+const ChannelCreation = ({ navigation }) => {
+    const { spinner } = useContext(ProgressContext);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const descriptionRef = useRef();
@@ -34,7 +37,17 @@ const ChannelCreation = () => {
         setTitle(title);
         setErrorMessage(title.trim() ? "" : "Please enter the title.");
     };
-    const _handleCreateButtonPress = () => {};
+    const _handleCreateButtonPress = async () => {
+        try {
+            spinner.start();
+            const id = await createChannel({ title, description });
+            navigation.replace("Channel", { id, title });
+        } catch (e) {
+            Alert.alert("Creation Error", e.message);
+        } finally {
+            spinner.stop();
+        }
+    };
     return (
         <KeyboardAwareScrollView
             contentContainerStyle={{ flex: 1 }}
